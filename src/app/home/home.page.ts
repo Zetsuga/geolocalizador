398bb2d565ddb74b0b496b2b205751d0039c1fd5
Zetsuga <darkbear24@gmail.com>
2022-03-10 19:12:02 +0100

    ionic geolocalizador

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from "@ionic-native/geolocation/ngx"
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public latitudPromesa;
+  public longitudPromesa;
+  public latitudObservable;
+  public longitudObservable;
+
+  constructor(private geolocation: Geolocation) {
+    this.latitudPromesa = 0;
+    this.longitudPromesa = 0;
+    this.latitudObservable = 0;
+    this.longitudObservable = 0;
+  }
+
+  public geolocalizarPromesas(){
+    console.log("entro")
+    this.geolocation.getCurrentPosition().then((resp)=>{
+      this.latitudPromesa = resp.coords.latitude;
+      this.longitudPromesa = resp.coords.longitude;
+      console.log(resp)
+    }).catch((error)=>{
+      console.log("error getting location",error);
+    })
+  }
+
+  public geolocalizarObservables(){
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data:any)=>{
+      this.latitudObservable = data.coords.latitude;
+      this.longitudObservable = data.coords.longitude;
+      console.log(data)
+    })
+  }
 
 }
